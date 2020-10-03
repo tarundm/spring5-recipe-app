e46ec19bab236c29ecb3be5529dea46bb83f8827
tarundm <tarun4deshmukh@gmail.com>
2020-10-03 22:08:06 +0530

    Added findByDescription in CategoryRepository and UnitOfMeasureRepository

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,5 +1,8 @@
 package guru.springframework.controllers;
 
+import guru.springframework.models.UnitOfMeasure;
+import guru.springframework.repositories.CategoryRepository;
+import guru.springframework.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,12 +10,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping(value = {"/", "/index"})
     public String getRecipes(Model model){
-
         model.addAttribute("recipeName", "Chicken Tikka Masala");
         System.out.println("Index Page");
+        System.out.println("Category ID is : " + categoryRepository.findByDescription("American").get().getId());
+        System.out.println("UoM ID Is : " + unitOfMeasureRepository.findByDescription("Teaspoon").get().getId());
         return "recipe";
-
     }
 }
