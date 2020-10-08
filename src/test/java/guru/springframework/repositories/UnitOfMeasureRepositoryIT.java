f1069a2e83c2a67ca4215e9265b38c286f86895d
tarundm <tarun4deshmukh@gmail.com>
2020-10-08 19:51:04 +0530

    Added changed for UnitOfMeasureRepositoryIT integration test. Also modified pom.xml to change version of maven-failsafe-plugin

--- src/test/java/guru/springframework/repositories/UnitOfMeasureRepositoryIT.java
+++ src/test/java/guru/springframework/repositories/UnitOfMeasureRepositoryIT.java
@@ -1,8 +1,8 @@
 package guru.springframework.repositories;
 
 import guru.springframework.models.UnitOfMeasure;
+import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -12,6 +12,11 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+// NOTE: We faces some error with Maven -> Verify which run both Unit test and integration test.
+// I removed version and SB automatically curated dependencies for us to 2.22.x version
+// Changed visibility of class and two test method to Public.
+// Changed "org.junit.jupiter.api.Test" import with "org.junit.Test"
+
 // Integration test run to Spring context and load it
 // @DataJpaTest will bring embedded databased and configure Spring Data JPA
 // Here we are testing DB with test so this becomes Integration test
@@ -20,24 +25,26 @@ import static org.junit.jupiter.api.Assertions.*;
 // If there are multiple test then first test will take time while others will use same spring app context.
 @RunWith(SpringRunner.class)
 @DataJpaTest
-class UnitOfMeasureRepositoryIntgTest {
+public class UnitOfMeasureRepositoryIT {
 
     @Autowired
     UnitOfMeasureRepository unitOfMeasureRepository;
 
+    UnitOfMeasureRepositoryIT uit;
+
     @BeforeEach
     void setUp() {
-
+        uit = new UnitOfMeasureRepositoryIT();
     }
 
     @Test
-    void findByDescription() {
+    public void findByDescription() {
         Optional<UnitOfMeasure> uom = unitOfMeasureRepository.findByDescription("Teaspoon");
         assertEquals("Teaspoon", uom.get().getDescription());
     }
 
     @Test
-    void findByDescriptionCup() {
+    public void findByDescriptionCup() {
         Optional<UnitOfMeasure> uom = unitOfMeasureRepository.findByDescription("Cup");
         assertEquals("Cup", uom.get().getDescription());
     }
